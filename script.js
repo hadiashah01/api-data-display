dde9ed6cff5483cfa3f8ddc1fc498fca9334de72
Hadia <hadiashah010@gmail.com>
2026-06-16 01:12:03 +0500

    style card elements using bootstrap utility classesin js file

--- script.js
+++ script.js
@@ -9,11 +9,11 @@
       class="card d-flex justify-content-center flex-column"
       style="flex-basis: 20%"
     >
-      <img class="card-img-top" alt="Card image cap" />
-      <h4 class="card-title">${element.name}</h4>
-      <p class="email"><strong>Email: </strong>${element.email}</p>
-      <p class="city"><strong>City: </strong>${element.address.city}</p>
-      <p class="website"><strong>Website: </strong>${element.website}</p>
+      <img class="card-img-top rounded-circle mx-auto" src="" alt="Card image cap" />
+      <h4 class="card-title text-center my-4 lh-base">${element.name}</h4>
+      <p class="email my-0 mx-auto lh-base mb-2"><strong>Email: </strong>${element.email}</p>
+      <p class="city my-0 mx-auto lh-base mb-2"><strong>City: </strong>${element.address.city}</p>
+      <p class="website my-0  mx-auto lh-base mb-2"><strong>Website: </strong>${element.website}</p>
       <a href="#" class="btn btn-warning">View Profile</a>
     </div>`;
       });
