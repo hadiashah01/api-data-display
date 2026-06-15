a9a4980a2632407914bf2af27fccddffcbf47995
Hadia <hadiashah010@gmail.com>
2026-06-16 01:00:15 +0500

    add 10 cards with img ,card-title ,3 p tags & btn as inner-html of container using forEach loop of user array
    the loop fetches each object's properties (name,email,city,website)

--- script.js
+++ script.js
@@ -3,6 +3,19 @@
       let data = await fetch(url);
       let users = await data.json();
       console.log(users);
+      users.forEach((element) => {
+        let  = document.querySelector(".container");
+        container.innerHTML += `<div
+      class="card"
+    >
+      <img class="card-img-top" alt="Card image cap" />
+      <h4 class="card-title">${element.name}</h4>
+      <p class="email"><strong>Email: </strong>${element.email}</p>
+      <p class="city"><strong>City: </strong>${element.address.city}</p>
+      <p class="website"><strong>Website: </strong>${element.website}</p>
+      <a href="#" class="btn btn-warning">View Profile</a>
+    </div>`;
+      });
     };
 
     response();
