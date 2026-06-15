f89d661cef020150ec965e1d3d2ba4c73543167c
Hadia <hadiashah010@gmail.com>
2026-06-16 02:29:20 +0500

    add h1 and .subtitle with styles &refactor styling for card elements and update font size for better readability

--- script.js
+++ script.js
@@ -10,10 +10,10 @@
       style="flex-basis: 20%"
     >
       <img class="card-img-top rounded-circle mx-auto" src="./assets/profile-img-${element.id}.png" alt="Card image cap" />
-      <h4 class="card-title text-center my-4 lh-base">${element.name}</h4>
-      <p class="email my-0 mx-auto lh-base mb-2"><strong>Email: </strong>${element.email}</p>
-      <p class="city my-0 mx-auto lh-base mb-2"><strong>City: </strong>${element.address.city}</p>
-      <p class="website my-0  mx-auto lh-base mb-2"><strong>Website: </strong>${element.website}</p>
+      <h4 class="card-title fw-semibold text-center my-3 lh-base">${element.name}</h4>
+      <p class="email my-0 mx-auto lh-sm mb-2"><strong>Email: </strong>${element.email}</p>
+      <p class="city my-0 mx-auto lh-sm mb-2"><strong>City: </strong>${element.address.city}</p>
+      <p class="website my-0  mx-auto lh-sm mb-2"><strong>Website: </strong>${element.website}</p>
       <a href="#" class="btn btn-warning">View Profile</a>
     </div>`;
       });
