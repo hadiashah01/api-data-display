ffbffc47a46cdde8b8b3f775e7b1333049373567
Hadia <hadiashah010@gmail.com>
2026-07-12 23:28:30 +0500

    add dynamic img of profiles and set text of .catchPhrase lowercase via javascript  in profile.script.js

--- profile.script.js
+++ profile.script.js
@@ -1,21 +1,24 @@
-
-    const profileURL = window.location.search;
-    const params = new URLSearchParams(profileURL);
-    const id = params.get("id");
-    const apiURL = `https://jsonplaceholder.typicode.com/users/${id}`;
-    let result = async () => {
-      const response = await fetch(apiURL);
-      const data = await response.json();
-      console.log(data);
-      let title = document.querySelector("title");
-      title.innerHTML = `${data.name}`;
-      const cardTitle = document.querySelector(".card-title");
-      cardTitle.innerText = data.name;
-      const cardText = document.querySelector(".card-text");
-      cardText.innerHTML = ` Hey there, I'm ${data.name}! My company's mission is <i>${data.company.catchPhrase}</i>. Below you can see my details & contact
+const profileURL = window.location.search;
+const params = new URLSearchParams(profileURL);
+const id = params.get("id");
+const apiURL = `https://jsonplaceholder.typicode.com/users/${id}`;
+let result = async () => {
+  const response = await fetch(apiURL);
+  const data = await response.json();
+  console.log(data);
+  let title = document.querySelector("title");
+  title.innerHTML = `${data.name}`;
+  const cardTitle = document.querySelector(".card-title");
+  cardTitle.innerText = data.name;
+  const img = document.querySelector(".card-img-top");
+  img.setAttribute("src", `./assets/profile-img-${data.id}.png`);
+  const cardText = document.querySelector(".card-text");
+  cardText.innerHTML = ` Hey there, I'm ${data.name}! My company's mission is <span class="catchPhrase"> <i>${data.company.catchPhrase}</i></span>. Below you can see my details & contact
           information`;
-      const listGroup = document.querySelector(".list-group");
-      listGroup.innerHTML = `
+  const catchPhrase = document.querySelector(".catchPhrase");
+  catchPhrase.style.textTransform = "lowercase";
+  const listGroup = document.querySelector(".list-group");
+  listGroup.innerHTML = `
           <li class="list-group-item"><span>Username :</span> ${data.username}</li>
         <li class="list-group-item">
           <span>Email :</span> ${data.email}
@@ -23,8 +26,7 @@
         <li class="list-group-item"><span>City :</span> ${data.address.city}</li>
         <li class="list-group-item"><span>Zipcode :</span> ${data.address.zipcode}</li>
         <li class="list-group-item"><span>Phone : </span>${data.phone}</li>
-        <li class="list-group-item"><span>Website :</span> ${data.website}</li>
+        <li class="list-group-item"><span>Website :</span> <a href="#">${data.website}</a></li>
         <li class="list-group-item"><span>Company :</span> ${data.company.name}</li>`;
-    };
-    result();
- 
+};
+result();
