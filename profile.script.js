b17997c529f702d49db8ed81014ac07eff1cf6bd
Hadia <hadiashah010@gmail.com>
2026-07-12 19:28:33 +0500

    refactor profile.html and add profile.script.js for dynamic user data fetching and display
    move js code from profile.html to profile.script.js and link it with profile.html

---
+++ profile.script.js
@@ -0,0 +1,30 @@
+
+    const profileURL = window.location.search;
+    const params = new URLSearchParams(profileURL);
+    const id = params.get("id");
+    const apiURL = `https://jsonplaceholder.typicode.com/users/${id}`;
+    let result = async () => {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      console.log(data);
+      let title = document.querySelector("title");
+      title.innerHTML = `${data.name}`;
+      const cardTitle = document.querySelector(".card-title");
+      cardTitle.innerText = data.name;
+      const cardText = document.querySelector(".card-text");
+      cardText.innerHTML = ` Hey there, I'm ${data.name}! My company's mission is <i>${data.company.catchPhrase}</i>. Below you can see my details & contact
+          information`;
+      const listGroup = document.querySelector(".list-group");
+      listGroup.innerHTML = `
+          <li class="list-group-item"><span>Username :</span> ${data.username}</li>
+        <li class="list-group-item">
+          <span>Email :</span> ${data.email}
+        </li>
+        <li class="list-group-item"><span>City :</span> ${data.address.city}</li>
+        <li class="list-group-item"><span>Zipcode :</span> ${data.address.zipcode}</li>
+        <li class="list-group-item"><span>Phone : </span>${data.phone}</li>
+        <li class="list-group-item"><span>Website :</span> ${data.website}</li>
+        <li class="list-group-item"><span>Company :</span> ${data.company.name}</li>`;
+    };
+    result();
+ 
